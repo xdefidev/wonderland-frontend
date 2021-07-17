035f41ec6394fbeeca37cab17ca32d7a548e39d7
victorelloveth <olaiyavf@Gmail.com>
2021-07-18 00:51:42 +0100

    update

--- packages/pancake-uikit/src/theme/colors.ts
+++ packages/pancake-uikit/src/theme/colors.ts
@@ -2,17 +2,17 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#1FC7D4",
-  primaryBright: "#53DEE9",
-  primaryDark: "#0098A1",
-  secondary: "#7645D9",
-  success: "#31D0AA",
+  primary: "#6d07bb",
+  primaryBright: "#6874f8",
+  primaryDark: "#6874f8",
+  secondary: "#6d07bb",
+  success: "#6874f8",
   warning: "#FFB237",
 };
 
 export const additionalColors = {
   binance: "#F0B90B",
-  overlay: "#452a7a",
+  overlay: ##6d07bb",
 };
 
 export const lightColors: Colors = {
@@ -58,7 +58,7 @@ export const darkColors: Colors = {
   invertedContrast: "#191326",
   input: "#372F47",
   inputSecondary: "#262130",
-  primaryDark: "#0098A1",
+  primaryDark: "#6874f8",
   tertiary: "#353547",
   text: "#F4EEFF",
   textDisabled: "#666171",
@@ -68,7 +68,7 @@ export const darkColors: Colors = {
     bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     inverseBubblegum: "linear-gradient(139.73deg, #3D2A54 0%, #313D5C 100%)",
     cardHeader: "linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)",
-    blue: "linear-gradient(180deg, #00707F 0%, #19778C 100%)",
+    blue: "linear-gradient(180deg, #00707F 0%, #6874f8 100%)",
     violet: "linear-gradient(180deg, #6C4999 0%, #6D4DB2 100%)",
     violetAlt: "linear-gradient(180deg, #434575 0%, #66578D 100%)",
     gold: "linear-gradient(180deg, #FFD800 0%, #FDAB32 100%)",
